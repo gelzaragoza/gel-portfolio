4c176329a15aaf3973b6e1539376ff7a5e327cbd
Gel <mangelicazaragoza@gmai.com>
2024-03-05 14:48:58 +0800

    fixed

--- pages/ParticlesBackground.js
+++ pages/ParticlesBackground.js
@@ -1,34 +1,38 @@
 import { useEffect } from 'react';
-import Particles from 'particles.js';
 
 const ParticlesBackground = () => {
   useEffect(() => {
-    Particles.init({
-      // Configure Particle.js settings here
-      selector: '.particles-background',
-      color: '#000000',
-      connectParticles: true,
-      responsive: [
-        {
-          breakpoint: 768,
-          options: {
-            maxParticles: 200,
-          }
-        },
-        {
-          breakpoint: 425,
-          options: {
-            maxParticles: 100,
-          }
-        },
-        {
-          breakpoint: 320,
-          options: {
-            maxParticles: 50,
-          }
-        }
-      ]
-    });
+    // Load Particles.js only in the client-side code
+    if (typeof window !== 'undefined') {
+      import('particles.js').then((particles) => {
+        particles.init({
+          // Configure Particle.js settings here
+          selector: '.particles-background',
+          color: '#000000',
+          connectParticles: true,
+          responsive: [
+            {
+              breakpoint: 768,
+              options: {
+                maxParticles: 200,
+              }
+            },
+            {
+              breakpoint: 425,
+              options: {
+                maxParticles: 100,
+              }
+            },
+            {
+              breakpoint: 320,
+              options: {
+                maxParticles: 50,
+              }
+            }
+          ]
+        });
+      });
+    }
   }, []);
 
   return <div className="particles-background" />;
