a7ad89d8845a111261e9fa5ebf5583c99415a53d
Gel <mangelicazaragoza@gmai.com>
2024-03-05 15:33:22 +0800

    fixed tsparticles

--- pages/ParticlesBackground.js
+++ pages/ParticlesBackground.js
@@ -18,23 +18,20 @@ const ParticlesBackground = ({ darkMode }) => {
           },
           particles: {
             number: {
-              value: 80,
+              value: 100,
               density: {
                 enable: true,
                 value_area: 800,
               },
             },
             color: {
-              value: darkMode ? "#ffffff" : "#000000",
+              value: darkMode ? "#ffffff" : "#7F7F7F",
             },
             shape: {
               type: "circle",
               stroke: {
                 width: 0,
                 color: darkMode ? "#000000" : "#ffffff",
-              },
-              polygon: {
-                nb_sides: 5,
               },
             },
             opacity: {
@@ -48,7 +45,7 @@ const ParticlesBackground = ({ darkMode }) => {
               },
             },
             size: {
-              value: 3,
+              value: 4,
               random: true,
               anim: {
                 enable: true,
@@ -57,69 +54,21 @@ const ParticlesBackground = ({ darkMode }) => {
                 sync: false,
               },
             },
-            line_linked: {
-              enable_auto: true,
-              distance: 150,
-              color: darkMode ? "#ffffff" : "#000000",
-              opacity: 0.4,
-              width: 1,
-              condensed_mode: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 600,
-              },
-            },
             move: {
               enable: true,
-              speed: 1,
-              direction: "none",
+              speed: 0.5,
+              direction: "bottom",
               random: false,
               straight: false,
               out_mode: "out",
               bounce: false,
-              attract: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 1200,
-              },
             },
           },
           interactivity: {
-            detect_on: "canvas",
             events: {
               onhover: {
-                enable: true,
-                mode: "grab",
-              },
-              onclick: {
-                enable: true,
-                mode: "push",
-              },
-              resize: true,
-            },
-            modes: {
-              grab: {
-                distance: 140,
-                line_linked: {
-                  opacity: 0.5,
-                },
-              },
-              bubble: {
-                distance: 400,
-                size: 40,
-                duration: 2,
-                opacity: 8,
-                speed: 3,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-              push: {
-                particles_nb: 4,
-              },
-              remove: {
-                particles_nb: 2,
+                enable: false,
+                mode: "repulse",
               },
             },
           },
