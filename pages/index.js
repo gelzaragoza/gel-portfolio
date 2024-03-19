0283f68de66aa974882ab8797c45bbefb45150a0
Gel <mangelicazaragoza@gmai.com>
2024-03-19 15:50:29 +0800

    updates

--- pages/index.js
+++ pages/index.js
@@ -142,7 +142,7 @@ export default function Home() {
               <h3 className=" text-rose-500 text-2xl py-1 pt-5 font-bold">
                 SERVICES I OFFER
               </h3>
-              <p className=" text-md py-2 leading-8  dark:text-white">
+              <p className=" text-md text-justify py-2 leading-8  dark:text-white">
                 Throughout my journey in the tech industry, I have honed the
                 skills to provide professional services for agencies and
                 startups that encompass compelling design, seamless web
@@ -280,7 +280,7 @@ export default function Home() {
           <section className="py-20 relative">
             <div>
               <h3 className="text-rose-500 text-2xl py-1 pt-5 font-bold">PORTFOLIO</h3>
-              <p className="text-md py-2 leading-8 dark:text-white">
+              <p className="text-md py-2 leading-8 dark:text-white text-justify">
                 Welcome to my digital gallery! Explore a curated collection showcasing the seamless blend of design and technology that fuels my passion. Each project is a testament to my dedication to creating visually stunning designs and seamless user experiences. Join me on this visual journey through my portfolio, a testament to my love for design and technology!{" "}
               </p>
             </div>
